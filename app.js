57a703e816a97db6f08f60529de479d25f0db9fd
Victoria Aoka <victoriaaoka@gmail.com>
2019-01-30 15:12:54 +0300

    make port dynamic

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const http = require('http');
-const router = require('./scripts/router.js');
+const http = require("http");
+const router = require("./scripts/router.js");
 const port = 8000;
 // create a web server.
 const server = http.createServer((request, response) => {
-	router.home(request, response);
-})
-server.listen(port);
-console.log('Server listening at localhost:8000');
+  router.home(request, response);
+});
+server.listen(process.env.PORT || port);
+console.log("Server listening at localhost:8000");
